356dc3f2f5e588e4feb75c12ddd96aa02a1e02db
Jorge Blanchard Cruz <rothgard@live.com>
2024-10-04 19:21:27 +0100

    inicio de componentes

---
+++ src/app/shared/heavy-loaders/heavy-loaders-slow.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  imports: [],
+  template: `
+    <h1>Heavy Loaders Slow</h1>
+  `,
+  styles: ``
+})
+export class HeavyLoadersSlowComponent {
+
+}
---
+++ src/app/shared/heavy-loaders/users-loader.component.ts
@@ -0,0 +1,15 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-users-loader',
+  standalone: true,
+  imports: [],
+  template: `
+    <h1>Users Loader</h1>
+  `,
+  styles: ``
+})
+
+export class UsersLoaderComponent {
+
+}
