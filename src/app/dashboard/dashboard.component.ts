208949fd0ac4a8193b4b12b386528a1f90a4e020
Jorge Blanchard Cruz <rothgard@live.com>
2024-10-05 18:28:22 +0100

    config de path alias en ts

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
-import { SidemenuComponent } from '../shared/sidemenu/sidemenu.component';
+
+import { SidemenuComponent } from '@shared/sidemenu/sidemenu.component';
+
 
 @Component({
   standalone: true,
