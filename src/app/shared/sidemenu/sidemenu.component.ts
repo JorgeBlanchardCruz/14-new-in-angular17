3bde5527c70f1f8b3f3873b401c207ee43c680ca
Jorge Blanchard Cruz <rothgard@live.com>
2024-10-05 17:51:27 +0100

    add pages, config de rutas y ui dashboard

--- src/app/shared/sidemenu/sidemenu.component.ts
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { routes } from '../../app.routes';
 
 @Component({
+  selector: 'app-sidemenu',
   standalone: true,
   imports: [],
   templateUrl: './sidemenu.component.html',
   styles: ``
 })
 export class SidemenuComponent {
+
+  public menuItems = routes
+    .map( route => route.children ?? [] )
+    .flat()
+    .filter ( route => route && route.path )
+    .filter ( route => !route.path?.includes(':') );
+
+  constructor() {
+
+    console.log(routes);
+
+  }
 
 }
