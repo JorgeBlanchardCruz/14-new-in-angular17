354cd79006fc5541aae749fc51687ef45eb0d462
Jorge Blanchard Cruz <rothgard@live.com>
2024-10-06 13:28:07 +0100

    lazy load de componentes

--- src/app/shared/heavy-loaders/heavy-loaders-slow.component.ts
+++ src/app/shared/heavy-loaders/heavy-loaders-slow.component.ts
@@ -1,13 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
+  selector: 'app-heavy-loaders-slow',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   template: `
-    <h1>Heavy Loaders Slow</h1>
+    <section [ngClass]="['w-full h-[600px]', cssClass]">
+
+      <h1>Heavy Loaders Slow</h1>
+
+    </section>
   `,
   styles: ``
 })
 export class HeavyLoadersSlowComponent {
+
+  @Input({ required: true }) cssClass!: string;
+
+  constructor() {
+    console.log('HeavyLoadersSlowComponent');
+
+    const start = Date.now();
+
+    while(Date.now() - start < 3000) { }
+
+    console.log('loaded');
+
+  }
 
 }
