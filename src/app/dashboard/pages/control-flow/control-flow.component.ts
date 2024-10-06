fc821ac7b504ab678f029c66245bd02d7df68737
Jorge Blanchard Cruz <rothgard@live.com>
2024-10-06 12:24:49 +0100

    nuevo control de flujos angular 18

--- src/app/dashboard/pages/control-flow/control-flow.component.ts
+++ src/app/dashboard/pages/control-flow/control-flow.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+
+
+export type Grade = 'A' | 'B' | 'F';
 
 @Component({
   standalone: true,
@@ -7,5 +10,16 @@ import { Component } from '@angular/core';
   styles: ``
 })
 export default class ControlFlowComponent {
+
+  public showContent = signal(false);
+  public grade = signal<Grade>('A');
+  public frameworks = signal<string[]>(['Angular', 'React', 'Vue', 'Svelte', 'Ember', 'Backbone', 'Knockout', 'Qwik']);
+  public frameworks2 = signal<string[]>([]);
+
+  public toogleContent() {
+    this.showContent.update( value => !value );
+  }
+
+
 
 }
--- src/app/shared/title/title.component.ts
+++ src/app/shared/title/title.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
+  selector: 'app-title',
   standalone: true,
   imports: [],
   templateUrl: './title.component.html',
   styles: ``
 })
 export class TitleComponent {
+
+    public title = signal('Title');
+
+    public setTitle(value: string) {
+      this.title.set(value);
+    }
 
 }
