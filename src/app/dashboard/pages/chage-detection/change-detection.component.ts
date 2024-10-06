bf9e8238b4e897f5ee39d22526f8919d303cd74b
Jorge Blanchard Cruz <rothgard@live.com>
2024-10-06 13:02:08 +0100

    mejor rendimiento con signals

--- src/app/dashboard/pages/chage-detection/change-detection.component.ts
+++ src/app/dashboard/pages/chage-detection/change-detection.component.ts
@@ -1,12 +1,63 @@
+import { provideRouter } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, signal } from '@angular/core';
+
+
+import { TitleComponent } from '@shared/title/title.component';
 
 @Component({
+  changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
-  imports: [CommonModule],
-  templateUrl: './change-detection.component.html',
+  imports: [CommonModule, TitleComponent],
+  template: `
+    <app-title [title]="currentFramework()" />
+
+    <pre> {{
+    frameworksAsProperty | json
+    }}</pre>
+
+    <pre> {{
+    frameworksAsSignal() | json
+    }}</pre>
+
+  `,
   styles: ``
 })
 export default class ChangeDetectionComponent {
+
+
+  public currentFramework = computed(
+    () => `Change detection - ${ this.frameworksAsSignal().name }`
+  );
+
+  public frameworksAsSignal = signal({
+    name: 'Angular',
+    releaseDate: '2016',
+  })
+
+  public frameworksAsProperty = ({
+    name: 'Angular',
+    releaseDate: '2016',
+  })
+
+
+  constructor() {
+
+    setTimeout(() => {
+      this.frameworksAsProperty.name = 'React';
+      this.frameworksAsProperty.releaseDate = '2013';
+
+       //El uso de Signals permiten un mucho mejor rendimiento en la detección de cambios
+      this.frameworksAsSignal.update( value => {
+        value.name = 'Vue';
+        value.releaseDate = '2014';
+
+        return { ...value };
+      });
+
+      console.log('hecho');
+    }, 3000);
+
+  }
 
 }
--- src/app/shared/title/title.component.ts
+++ src/app/shared/title/title.component.ts
@@ -4,7 +4,7 @@ import { booleanAttribute, Component, Input } from '@angular/core';
   selector: 'app-title',
   standalone: true,
   imports: [],
-  template: '<h1 class="text-3xl mb-5">{{ title }} - {{ withShadow }}</h1>',
+  template: '<h1 class="text-3xl mb-5">{{ title }}</h1>',
   styles: ``
 })
 export class TitleComponent {
